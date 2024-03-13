0cae1b15dee100d3b9a63dc5943dc436b7a551cc
dkatsios <d.katsios@gmail.com>
2024-03-13 10:58:55 +0900

    added css files. added tab functionality to review

--- code_snippet_gen/static/review.js
+++ code_snippet_gen/static/review.js
@@ -20,3 +20,33 @@ document.getElementById("runButton").onclick = async () => {
         console.error('Failed to execute the snippet.');
     }
 };
+
+document.getElementById('codeSnippet').addEventListener('keydown', function(e) {
+    if (e.key === 'Tab') {
+        e.preventDefault();  // Prevent the default tab behavior (focus change)
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        // Insert 4 spaces on TAB, remove 4 spaces on Shift+TAB
+        if (!e.shiftKey) {
+            // Set textarea value to: text before caret + 4 spaces + text after caret
+            this.value = this.value.substring(0, start) + "    " + this.value.substring(end);
+
+            // Move the caret to the end of the inserted spaces
+            this.selectionStart = this.selectionEnd = start + 4;
+        } else {
+            // If the Shift key is also pressed, remove 4 spaces
+            var beforeCaret = this.value.substring(0, start);
+            
+            // Check if the last 4 characters are spaces and if so, remove them
+            if (beforeCaret.endsWith("    ")) {
+                this.value = this.value.substring(0, start - 4) + this.value.substring(start);
+                // Move the caret to the start position minus 4 (after removing the spaces)
+                this.selectionStart = this.selectionEnd = start - 4;
+            } else {
+                // If there are not 4 spaces, just move the caret back one position
+                this.selectionStart = this.selectionEnd = Math.max(start - 1, 0);
+            }
+        }
+    }
+});
